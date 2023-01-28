abbc26c56f98b9ae013595b5a1c189570a1ec805
fakrul-hossain <fakrulhossain912@gmial.com>
2023-01-28 14:07:23 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// random number
+// change the number(variable) value and watch the result in browser console
+let number = 79;
+// if else condition
+if (number >= 80) {
+    console.log('You have got A+');
+}
+else if (number >= 70) {
+    console.log('You have got A')
+}
+else if (number >= 60) {
+    console.log('You have got A-')
+}
+else if (number >= 50) {
+    console.log('You have got B')
+}
+else if (number >= 40) {
+    console.log('You have got C')
+}
+else if (number >= 33) {
+    console.log('You have got D (TTP Pass)')
+}
+else if (number >= 0) {
+    console.log('You are fail')
+}
